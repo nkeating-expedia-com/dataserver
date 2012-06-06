1025da3cba3f0d87abaacb20d52c37b1f439f728
Petrovsky Alexander <askjuise@gmail.com>
2012-06-06 14:15:11 +0800

    simple submitter

---
+++ src-java/ru/juise/submitter/Receiver.java
@@ -0,0 +1,41 @@
+package ru.juise.submitter;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+
+public class Receiver {
+	private String queue = "echoq";
+
+	private String host = "localhost";
+	private Integer port = 5672;
+
+	public Receiver(String queue, String host, Integer port) {
+		this.queue = queue;
+		this.host = host;
+		this.port = port;
+	}
+
+	public void receive() throws Exception {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost(host);
+		connectionFactory.setPort(port);
+
+		Connection connection = connectionFactory.newConnection();
+
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(queue, false, false, false, null);
+
+		QueueingConsumer queueConsumer = new QueueingConsumer(channel);
+		channel.basicConsume(queue, true, queueConsumer);
+
+		QueueingConsumer.Delivery delivery;
+		while (true) {
+			delivery = queueConsumer.nextDelivery();
+
+			System.out.println("Receiver - " + new String(delivery.getBody()));
+		}
+	}
+}
---
+++ src-java/ru/juise/submitter/Sender.java
@@ -0,0 +1,36 @@
+package ru.juise.submitter;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+
+public class Sender {
+	private String queue = "echoq";
+
+	private String host = "localhost";
+	private Integer port = 5672;
+
+	public Sender(String queue, String host, Integer port) {
+		this.queue = queue;
+		this.host = host;
+		this.port = port;
+	}
+
+	public void send(String msg) throws Exception {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost(host);
+		connectionFactory.setPort(port);
+
+		Connection connection = connectionFactory.newConnection();
+
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(queue, false, false, false, null);
+
+		channel.basicPublish("", queue, null, msg.getBytes());
+		System.out.println("Sender - Ok.");
+
+		channel.close();
+		connection.close();
+	}
+}
