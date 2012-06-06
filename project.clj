a8dc77ccddf553ece22bf3307e24b48a4ab98d68
Nikita Prokopov <prokopov@aboutecho.com>
2012-06-06 14:14:43 +0400

    Json lib added

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [storm "0.7.2"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/data.xml  "0.0.4"]
-                 [com.rabbitmq/amqp-client "2.8.2"]]
+                 [com.rabbitmq/amqp-client "2.8.2"]
+                 [com.googlecode.json-simple/json-simple "1.1"]]
   :jvm-opts     ["-Dfile.encoding=UTF-8"
                  "-Xmx512M"])
                  ; "-Djava.library.path=/System/Library/Java/Extensions:/usr/local/lib:/opt/local/lib:/usr/lib"])
